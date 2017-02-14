c81a6e2050a6e03b7702e626863d15c48cc7e0f2
Eccenux <eccenux+git@gmail.com>
2017-02-14 23:25:10 +0100

    generate

---
+++ generate.php
@@ -0,0 +1,53 @@
+<?php
+
+$recordsNum = 1 * 1000;
+
+class ArrayGenerator
+{
+	public $array = null;
+	public $arrayCount = null;
+	
+	function __construct ($array)
+	{
+		$this->array = $array;
+		$this->arrayCount = count($this->array);
+	}
+	function generate($i) {
+		return $this->array[$i % $this->arrayCount];
+	}
+}
+
+class MethodGenerator
+{
+	function __construct ()
+	{
+		$this->methods = new ArrayGenerator(array('GET', 'POST', 'PUT', 'DELETE'));
+	}
+	function generate($i) {
+		return $this->methods->generate($i);
+	}
+}
+$methodGenerator = new MethodGenerator();
+
+class UrlGenerator
+{
+	function __construct ()
+	{
+		$this->dirs = new ArrayGenerator(array('circulation', 'inventory', 'settings'));
+		$this->resources = new ArrayGenerator(array('reader', 'holding', 'document', 'periodical', 'loss'));
+	}
+	function generate($i) {
+		return "/".$this->dirs->generate($i / ($this->resources->arrayCount + 2))."/".$this->resources->generate($i);
+	}
+}
+$urlGenerator = new UrlGenerator();
+
+function ipGenerator($i) {
+	return (($i/(256*256*256))%256).".".(($i/(256*256))%256).".".(($i/256)%256).".".($i%256);
+}
+
+$fp = fopen("insert_$recordsNum.sql", 'w');
+for ($i = 0; $i < $recordsNum; $i++) {
+	fwrite($fp, "\nINSERT INTO alog (ip,deltaT,method,url) VALUES ('".ipGenerator($i)."',".($i).",'".$methodGenerator->generate($i)."','".$urlGenerator->generate($i)."'); ");
+}
+fclose($fp);
