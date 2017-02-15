7a1422cf4b038619eb0e364ca29182e2c3df9bfb
Eccenux <eccenux+git@gmail.com>
2017-02-15 19:00:32 +0100

    psql bat instead of path change

---
+++ psql.bat
@@ -0,0 +1,2 @@
+@echo off
+"c:\Program Files\PostgreSQL\9.6\bin\psql" %*
--- test.ps1
+++ test.ps1
@@ -1,22 +1,26 @@
-powershell
+# powershell
 
-rem INIT
-set PATH=%PATH%;"c:\Program Files\PostgreSQL\9.6\bin\"
+# INIT -> psql.bat
+# set PATH=%PATH%;"c:\Program Files\PostgreSQL\9.6\bin\"
 
-rem INSERT MS
+# INSERT MS
 osql -E -d test -i create.ms.sql
-Measure-Command {osql -E -d test -i insert_1000000.sql}
+Measure-Command {osql -E -d test -i insert_1000000.sql -o insert.ms.log}
 
-rem INSERT PG
-psql -U postgres --port 5433 -d test -f create.pg.sql
-Measure-Command {psql -U postgres --port 5433 -d test -f insert_1000000.sql}
+# INSERT PG
+.\psql -U postgres --port 5433 -d test -f create.pg.sql
+Measure-Command {.\psql -U postgres --port 5433 -d test -f insert_1000000.sql > insert.pg.log}
 
+# Check
+osql -E -d test -Q "SELECT count(*) FROM alog"
+.\psql -U postgres --port 5433 -d test -c "SELECT count(*) FROM alog"
 
-rem ms/pg int
-Measure-Command {psql -U postgres --port 5433 -d test -c "update alog set deltaT = 0"}
+# ms/pg int
+Measure-Command {osql -E -d test -Q "update alog set deltaT = 0"}
+Measure-Command {.\psql -U postgres --port 5433 -d test -c "update alog set deltaT = 0"}
 
-rem pg text
+# pg text
 update alog set url = left(url, 10)
 
-rem ms text
+# ms text
 update alog set url = left(cast(url as varchar), 10)
