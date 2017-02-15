c79288727658ab87c94d750e808f3e32e531731d
Eccenux <eccenux+git@gmail.com>
2017-02-15 23:19:26 +0100

    final commands

--- test.ps1
+++ test.ps1
@@ -3,24 +3,33 @@
 # INIT -> psql.bat
 # set PATH=%PATH%;"c:\Program Files\PostgreSQL\9.6\bin\"
 
+##
+# INSERT
+##
+
 # INSERT MS
 osql -E -d test -i create.ms.sql
-Measure-Command {osql -E -d test -i insert_1000000.sql -o insert.ms.log}
+Measure-Command {osql -E -d test -i insert_1000000.ms.sql -o insert.ms.log}
 
 # INSERT PG
 .\psql -U postgres --port 5433 -d test -f create.pg.sql
 Measure-Command {.\psql -U postgres --port 5433 -d test -f insert_1000000.sql > insert.pg.log}
 
+##
 # Check
+##
 osql -E -d test -Q "SELECT count(*) FROM alog"
 .\psql -U postgres --port 5433 -d test -c "SELECT count(*) FROM alog"
 
+##
+# UPDATE
+##
 # ms/pg int
-Measure-Command {osql -E -d test -Q "update alog set deltaT = 0"}
-Measure-Command {.\psql -U postgres --port 5433 -d test -c "update alog set deltaT = 0"}
+Measure-Command {osql -E -d test -Q "update alog set deltaT = 0" -o update.ms.log}
+Measure-Command {.\psql -U postgres --port 5433 -d test -c "update alog set deltaT = 0" > update.pg.log}
 
 # pg text
-update alog set url = left(url, 10)
+Measure-Command {.\psql -U postgres --port 5433 -d test -c "update alog set url = left(url, 10)" > updatetxt.pg.log}
 
 # ms text
-update alog set url = left(cast(url as varchar), 10)
+Measure-Command {osql -E -d test -Q "update alog set url = left(cast(url as varchar), 10)" -o updatetxt.ms.log}
